e4616a9bc1aa4cdec8b7dcd66e783890a4f339bd
Leonardo Matrone <50384620+lmatrone@users.noreply.github.com>
2019-06-16 15:13:49 -0300

    Update connectToSmartContract.js

--- connectToSmartContract.js
+++ connectToSmartContract.js
@@ -29,14 +29,14 @@ function waitForTxToBeMined(txHash, objStatus) {
         console.log(txReceipt); 
         if (txReceipt) {
             if (txReceipt.status == "0x1") {
-                $(objStatus).css("background-color", "LawnGreen");
+                $(objStatus).css("background-color", "LawnWhite");
                 $(objStatus).html("Registrado com sucesso no bloco: " + txReceipt.blockNumber + " - Hash: " + txHash);            
             } else {
                 $(objStatus).css("background-color", "Salmon");
                 $(objStatus).html("There was an error - Transaction hash: " + txHash + " final status: " + txReceipt.status);            
             }
         } else {
-            $(objStatus).css("background-color", "yellow");
+            $(objStatus).css("background-color", "white");
             $(objStatus).text("Aguardando mineração. Hash: " + txHash);
             window.setTimeout(waitForTxToBeMined, 1500, txHash, objStatus);
         }
